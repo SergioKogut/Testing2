fec509fe4627cbb34281f83743be1261a7e50160
Сергій Когут <sergio.kogut@gmail.com>
2018-07-08 11:47:45 +0300

    next

--- Testing2/Form1.Designer.cs
+++ Testing2/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTestNLog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTestNLog
+            // 
+            this.btnTestNLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTestNLog.Location = new System.Drawing.Point(554, 41);
+            this.btnTestNLog.Name = "btnTestNLog";
+            this.btnTestNLog.Size = new System.Drawing.Size(178, 39);
+            this.btnTestNLog.TabIndex = 0;
+            this.btnTestNLog.Text = "Тест логування";
+            this.btnTestNLog.UseVisualStyleBackColor = true;
+            this.btnTestNLog.Click += new System.EventHandler(this.btnTestNLog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(28, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Текст що передається в Log\r\n";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.txtLog.Location = new System.Drawing.Point(45, 86);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(235, 26);
+            this.txtLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTestNLog);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTestNLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- Testing2/Form1.cs
+++ Testing2/Form1.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace Testing2
 {
     public partial class Form1 : Form
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public void MyMethod1()
+        {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method
+            // and pass log level as the parameter.
+            logger.Log(LogLevel.Info, "Sample informational message");
+
+
+            // Example of logging an exception
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "ow noos!"); // render the exception with ${exception}
+                throw;
+            }
+
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTestNLog_Click(object sender, EventArgs e)
+        {
+            //  MyMethod1();
+            logger.Info("User  click on button test" + txtLog.Text);
         }
     }
 }
